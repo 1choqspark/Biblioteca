21a38d64d46b6edb69934ff8fa70eddb2a9fd94b
1Choq <58182184+1choqspark@users.noreply.github.com>
2024-08-16 13:22:03 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!--Footer-->
+<div class="footer" style="background-color:rgb(0, 0, 0);  width: 100%;">
+  <div style="background-color:rgb(0, 0, 0); color:white; margin-left: 140px;">
+    <h4>Contact: Ing. Antonio Pérez Vázquez </h4>
+    <p>
+      
+      <brInstituto Mexicano de Capacitación y Certificación />
+      <a href="mailto:antonio.pe.v@gmail.com">antonio.pe.v@gmail.com</a><br>
+      <br />
+      Departamento de desarrollo
+    </p>
+  </div>
+  <div style="background-color:rgb(33, 150, 243);">
+    <div id="newsletter">
+      <div align="right" style="color:white; margin-right: 150px;">
+        <p><img src="images/logo3.jpeg" width="300" height="50" /><br />
+          Instituto Mexicano de Capacitación y Certificación</p>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/lumen/bootstrap.min.css">
+<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Biblioteca virtual</title>
+  <link  rel = "shortcut icon"  href = "images/icon.jpeg"  type = "image / x-icon" >
+  <style>
+    .nav-link {
+        font-size: 20px; /* Puedes ajustar el tamaño según tus necesidades */
+    }
+</style>
+</head>
+<nav class="navbar navbar-expand-lg " data-bs-theme="dark" style="background-color: black;">
+  <div class="container-fluid">
+    <a href="index.php" class="navbar-brand animate__animated animate__tada "><img src="images/logo.jpg" style="width: 250px;"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="active"><a class="nav-link active" href="index.php">Inicio</a></li>
+        <li><a class="nav-link" href="https://imcescuelasuperior.com.mx/">Sitio oficial</a></li>
+        <li> <a class="nav-link" href="https://imcescuelasuperior.com.mx/contacto-imc">Contacto</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body>
+<div style="background-color: rgb(33, 150, 243); height: 40px; text-align: center;">
+  <h5 style="color: white; font-family: Lucida Calligraphy; margin: 0;">
+    Instituto Mexicano de Capacitación y Certificación
+  </h5>
+</div>
