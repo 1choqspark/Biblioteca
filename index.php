0edfc83ef1660732ea753108738115ccdcfd2821
1Choq <58182184+1choqspark@users.noreply.github.com>
2024-08-16 13:21:16 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    include("header.php");
+?>
+<br>
+    <h2 align="center" style="font-family: Lucida Calligraphy";><strong>Acceso a Biblioteca Virtual<br />
+      </strong></h2>
+    <blockquote>
+      
+</p>
+<p>&nbsp;</p>
+
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-11">
+      <div class="card card border-dark">
+        <div class="card-header" style="background-color:rgb(33, 150, 243); color:white; font-family: Lucida Calligraphy;"><h4 class="card-title"><strong>Contenido de material digitalizado</strong></h4></div>
+        <div class="card-body">
+          <h5 class="card-title">Topics of interest include but are not limited to:</h5>
+          <p class="card-text">
+            <ul>
+              <li><a href="docs/libroICOKG2023.pdf">-- Book ICOKG 2023</a></li>
+              <li><a class="dropdown-item" href="https://biblioteca.ucm.es/data/cont/media/www/pag-21364/Guia%20NNN%20Consult%20Planes.pdf">
+              Taxonomías NANDA NIC NOC</a></li>
+              <li>Reasoning</li>
+              <li>Ontologies</li>
+              <li>Ontology engineering</li>
+              <li>Web semantic</li>
+              <li>Graphs of knowledge</li>
+              <li>Text mining</li>
+              <li>Extraction of information</li>
+              <li>Modeling of topics</li>
+              <li>Natural Language Processing</li>
+              <li>Ontology learning</li>
+              <li>Retrieval of information</li>
+              <li>Machine learning</li>
+              <li>Data science applications</li>
+              <li>Applications</li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</blockquote>
+<p>&nbsp;</p>
+<!--Footer-->
+<?php
+    include("footer.php");
+?>
